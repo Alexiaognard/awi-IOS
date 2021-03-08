86e5cc41c1e889cc08cda3c0b970503f4ab4e5d9
Alexiaognard <ognardalexiats4@gmail.com>
2021-03-08 11:30:00 +0100

    Add xcode project

---
+++ AWI-FestivalJeux/AWI-FestivalJeux/AWI_FestivalJeuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWI_FestivalJeuxApp.swift
+//  AWI-FestivalJeux
+//
+//  Created by Alexia Ognard on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AWI_FestivalJeuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWI-FestivalJeux/AWI-FestivalJeux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AWI-FestivalJeux
+//
+//  Created by Alexia Ognard on 08/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
