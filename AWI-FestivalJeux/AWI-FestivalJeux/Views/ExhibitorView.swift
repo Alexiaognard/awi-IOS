ecfff24c7b9ea50fb6e8f8fa20ec1442a30952a4
Alexiaognard <ognardalexiats4@gmail.com>
2021-03-11 12:13:26 +0100

    Add : Views (empty)

---
+++ AWI-FestivalJeux/AWI-FestivalJeux/Views/ExhibitorView.swift
@@ -0,0 +1,20 @@
+//
+//  ExhibitorView.swift
+//  AWI-FestivalJeux
+//
+//  Created by Alexia Ognard on 11/03/2021.
+//
+
+import SwiftUI
+
+struct ExhibitorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExhibitorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExhibitorView()
+    }
+}
---
+++ AWI-FestivalJeux/AWI-FestivalJeux/Views/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  AWI-FestivalJeux
+//
+//  Created by Alexia Ognard on 11/03/2021.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
